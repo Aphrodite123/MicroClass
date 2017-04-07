8f144c6f20b123ca87fc9428b85798e79e7b7c1a
mingyang <342709657@qq.com>
2017-04-07 12:08:51 +0800

    测试

--- app/src/main/java/com/aphrodite/microclass/MainActivity.java
+++ app/src/main/java/com/aphrodite/microclass/MainActivity.java
@@ -9,6 +9,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        intiVIew();
+    }
+
+    private void intiVIew() {
 
     }
 }
