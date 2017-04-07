f65ae6cdfd73e5e1bdfb373fa401cc2548301ea3
mingyang <342709657@qq.com>
2017-04-07 12:12:48 +0800

    测试

---
+++ app/src/main/java/com/aphrodite/microclass/widget/AutoImageView.java
@@ -0,0 +1,63 @@
+package com.aphrodite.microclass.widget;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.support.v7.widget.AppCompatImageView;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+/**
+ * Created by smy on 2017/4/7 0007.
+ */
+
+public class AutoImageView extends AppCompatImageView {
+
+    protected Paint mPaint;
+
+    protected boolean isPressed = false;
+
+    public AutoImageView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    }
+
+    public AutoImageView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public AutoImageView(Context context) {
+        super(context);
+        mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    }
+
+
+    public boolean onTouchEvent(MotionEvent event) {
+        isPressed = true;
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                mPaint.setColor(0x60636363);
+                break;
+            case MotionEvent.ACTION_UP:
+                mPaint.setColor(0x00000000);
+                break;
+            case MotionEvent.ACTION_CANCEL:
+                mPaint.setColor(0x00000000);
+                break;
+            default:
+                break;
+        }
+        invalidate();
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        if (isPressed && isClickable() && isEnabled()) {
+            canvas.drawRoundRect(new RectF(0, 0, getWidth(), getHeight()),
+                    0, 0, mPaint);
+        }
+    }
+}
