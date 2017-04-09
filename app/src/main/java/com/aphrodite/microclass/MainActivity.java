b810d8015edcafa3d9c1cc5b38a9e2086e605651
Aphrodite123 <927467936@qq.com>
2017-04-09 10:08:59 +0800

    设置沉浸式状态栏

--- app/src/main/java/com/aphrodite/microclass/MainActivity.java
+++ app/src/main/java/com/aphrodite/microclass/MainActivity.java
@@ -1,11 +1,16 @@
 package com.aphrodite.microclass;
 
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.view.Gravity;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.aphrodite.microclass.base.BaseActivity;
 import com.aphrodite.microclass.ui.adapter.TabPageIndicatorAdapter;
@@ -26,8 +31,20 @@ public class MainActivity extends BaseActivity {
     HeadView headView;
     // 抽屉导航布局
     @BindView(R.id.drawer_layout)
-     DrawerLayout drawerLayout;
+    DrawerLayout drawerLayout;
     List<String> topName = Arrays.asList("头条", "视频", "开心一刻", "浏览记录");
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //设置透明状态栏
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window window = getWindow();
+            window.setFlags(
+                    WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS,
+                    WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
+    }
 
     @Override
     public int getContentView() {
